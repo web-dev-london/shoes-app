eaf9fdf38b77e205e39919d0ab83c523f5e58aa1
Murod Akhmedov <murod.london@gmail.com>
2024-07-07 16:56:30 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { AiFillStar } from "react-icons/ai";
+
+
+function App() {
+
+    return (
+        <>
+            <AiFillStar className="rating-star" />
+        </>
+    )
+}
+
+export default App
